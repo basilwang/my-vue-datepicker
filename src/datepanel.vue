8e21ab0ca3f2c15e331fa7bc8136b50f616e5532
basilwang <wanghuajie@yunwii.com>
2017-03-02 20:26:52 +0800

    完成了简单的日历

--- src/datepanel.vue
+++ src/datepanel.vue
@@ -1,25 +1,22 @@
 <template>
-   <div class="date">
-        <div class="set-out-date">
+    <div>
           <span class="title">出发日期<br><i>Set&nbsp;out&nbsp;Date</i></span>
           <input id="setOutDate" type="text" @focus="isActive=true" name="" value="2016-10-01 星期一">
           <i class="date-btn prev"></i>
           <i class="date-btn next"></i>
-          <datepicker></datepicker>
-        </div>
-        <div class="return-date disabled">
-          <span class="title">返程日期<br><i>Return&nbsp;Date</i></span>
-          <input id="returnDate" type="text" name="" value="2016-10-03 星期三">
-          <datepicker></datepicker>
-        </div>
-   </div>
-
+          <datepicker :isActive="isActive"></datepicker>
+    </div>
 </template>
 
 <script>
   var Vue=require('vue');
   module.exports = {
     name:'datepanel',
+    data:function(){
+         return {
+           isActive:false
+         };
+    },
     components:{datepicker:require('./datepicker.vue')}
     
   };
--- src/datepicker.vue
+++ src/datepicker.vue
@@ -2,7 +2,7 @@
 
      
 
-   <div id="calendar" class="calendar-wrap"  :style="styleSheets" :class="{open:isActive}">
+   <div id="calendar" class="calendar-wrap"  :style="styleSheets" :class="{open:isDatePickerActive}">
   		<!-- 按钮 -->
   		<i id="prev" @click="prevFn()" class="prev"></i>
   		<i id="next" @click="nextFn()" class="next"></i>
@@ -35,25 +35,73 @@
   var Vue=require('vue');
   module.exports = {
     name:'datepicker',
+    props:['isActive'],
     data: function () {
       var today=new Date();
       return {
-         isActive:false,
          year:today.getFullYear(),
          month:today.getMonth(),
          date:today.getDate(),
          selected:'',
-         weekList:['日', '一', '二', '三','四','五','六'],
-         monthList:[]
+         weekList:['日', '一', '二', '三','四','五','六']
 
       };
     },
-    mounted:function(){
-         this.styleSheets={
-         	top: this.$parent.offsetTop + 50 + 'px',
-         	left: '20px'
-         };
+    computed:{
+    	isDatePickerActive:function(){
+    		return this.isActive
+    	},
+    	//只做日历数组
+  			daysList:function() {
+  				//返回每个月的日长
+  				var currentMonthLength = new Date(this.year, this.month + 1, 0).getDate(),
+  					dateObj = [],
+  					dateList = [];
+
+  				//确定当月的第一天是星期几
+        		var startDay = new Date(this.year, this.month, 1).getDay();
+  				//先将当月日期塞入数组中
+  				for (var i = 0; i < currentMonthLength; i++) {
+  					dateObj[i] = i+1
+  				}
+        		
+      		//补全剩余位置
+      		for (var j = 0; j <= 35; j++) {
+      			if(j < startDay){
+      				dateObj = [''].concat(dateObj)
+      			}
+      			if(j > startDay + currentMonthLength){
+      				dateObj = dateObj.concat('')
+      			}
+      		}
+
+      		//以7天为一组从新组合一个二维日期数组
+            var timeStep = Math.ceil(dateObj.length / 7);
+            for(var i = 0; i < timeStep; i++){
+                var stepArr = [];
+                var stepRank = dateObj.length < 7 ? dateObj.length : 7;
+                
+                stepArr = dateObj.splice(0, stepRank);
+                
+                dateList.push(stepArr);
+            }
+            console.log(dateList)
+        		return dateList
+  			},
+  			//表头语言控制
+        title:function(){
+
+            var month = {1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六',7: '七', 8: '八', 9: '九', 10: '十', 11: '十一', 12: '十二'}[this.month+1]
+             return this.year + '年' + month + '月';
+                 
+          }
     },
+    // mounted:function(){
+    //      this.styleSheets={
+    //      	top: this.$parent.$el.offsetTop + 50 + 'px',
+    //      	left: '20px'
+    //      };
+    // },
     methods: {
         //点击prev按钮
   		prevFn:function(){
