dd16d75d2c5f1ae8e89fad5fac15431ebeaa4355
basilwang <wanghuajie@yunwii.com>
2017-03-05 09:28:18 +0800

    增加了Clickoutside directive，点击页面其他区域date picker会隐藏，从这个细节大家可以看到组件化的威力

--- src/datepanel.vue
+++ src/datepanel.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <span class="title">{{title}}<br><i>{{desc}}</i></span>
-    <input id="setOutDate" @focus="isActive=true" type="text" name="" :value="selected">
+    <input id="setOutDate" @focus="isActive=true" v-clickoutside="handleClose" type="text" name="" :value="selected">
     <i class="date-btn prev"></i>
     <i class="date-btn next"></i>
-    <datepicker :isActive="isActive" @date="selectDate"></datepicker>
+    <datepicker :isActive="isActive" ref="reference" @date="selectDate"></datepicker>
   </div>
 </template>
 <script>
 var datepicker = require('./datepicker.vue');
+var Clickoutside = require('./utils/clickoutside.js');
 module.exports = {
   name: 'datepanel',
   props: ['title', 'desc'],
@@ -17,13 +18,23 @@ module.exports = {
       console.log('在父组件接受的值' + date);
       this.selected = date;
       this.isActive = false;
-    }
+    },
+    handleClose: function () {
+      this.isActive = false;
+    },
   },
   data: function () {
     return {
       isActive: false,
+      popperElm: null,
       selected: ''
     };
+  },
+  directives: {
+    Clickoutside: Clickoutside
+  },
+  mounted: function () {
+    this.popperElm = this.$refs.reference.$el;
   },
   components: {
     datepicker: datepicker
--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ var datepanel = require('./datepanel.vue');
 var helloworld = require('./helloworld.vue');
 
 var app = new Vue({
-    el: '#app',
-    components: { citypicker: citypicker, helloworld: helloworld, datepanel: datepanel }
+  el: '#app',
+  components: { citypicker: citypicker, helloworld: helloworld, datepanel: datepanel }
 });
-//alert(app.message);
-//alert(citypicker.fromCity);
+// alert(app.message);
+// alert(citypicker.fromCity);
 
-//alert('hello world');
+// alert('hello world');
